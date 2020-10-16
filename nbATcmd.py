1e17f0a1a46a094eaebe1de167d234a5faf1afe8
wangyv <wangyv6@outlook.com>
2020-10-16 05:17:21 -0900

    nop

--- nbATcmd.py
+++ nbATcmd.py
@@ -1,14 +1,15 @@
-from mUtils import serialSendMsg  ,readConf 
+from mUtils import serialSendMsg  ,readConf ,getSerial
 import time
 
 
+ser = getSerial()
 AT_CMD = ["AT+CGREG?","AT+MIPLCREATE"]
 def signalCallBack():
     print("signal too week")
 
 def getSignal():  
     r""" 获得信号强度 """
-    ret = serialSendMsg('AT+CSQ')
+    ret = serialSendMsg('AT+CSQ',ser=ser)
     
     return int(ret[1])
 
@@ -21,5 +22,9 @@ def initConnect():
         time.sleep(1)
         signal = getSignal()    
 
-    
+    serialSendMsg('AT+COPS=1,2,"46000"') #//设置手动注册移动运营MNC
+    serialSendMsg('AT+CSCON=1') #//打开信号提示自动上报。
+    serialSendMsg('AT+CGDCONT?') # 打开注册信息自动上报
+    serialSendMsg('AT+MIPLCREATE')
+    serialSendMsg()
 
